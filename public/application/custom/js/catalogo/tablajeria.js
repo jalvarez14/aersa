a1052277cac15b69e7b6a6d44fc21b7f88f935e7
Daniel Castanedo Barragan <daniel.b@hostdime.com.mx>
2016-04-25 17:42:24 -0500

    aersa

--- public/application/custom/js/catalogo/tablajeria.js
+++ public/application/custom/js/catalogo/tablajeria.js
@@ -66,7 +66,7 @@
                 datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
                 queryTokenizer: Bloodhound.tokenizers.whitespace,
                 remote: {
-                  url: '/catalogo/tablajeria/getproducts?q=%QUERY',
+                  url: '/autocomplete/getproductos?q=%QUERY',
                   wildcard: '%QUERY'
                 }
               });
@@ -83,41 +83,7 @@
                   $('input[name=idproducto]').val(suggestion.id);
               });
               
-            var data = new Bloodhound({
-                datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
-                queryTokenizer: Bloodhound.tokenizers.whitespace,
-                remote: {
-                  url: '/catalogo/tablajeria/getproductos?q=%QUERY',
-                  wildcard: '%QUERY'
-                }
-            });
-            
-            $('input#producto_autocomplete').typeahead(null, {
-                name: 'best-pictures',
-                display: 'value',
-                hint: true,
-                highlight: true,
-                source: data,
-                limit:5,
-            });
-            
-            $('input#producto_autocomplete').bind('typeahead:select', function(ev, suggestion) {
-                $('#producto_add').attr('disabled',false);
-            });
-
-            var count = 0;
-            $('#producto_add').on('click', function () {
-                var tr = $('<tr>');
-                tr.append('<td>Producto 1</td>');
-                tr.append('<td><i class="fa fa-trash"></i></td>');
-                $('#productos_table tbody').append(tr);
-                count++;
-                $('.fa-trash').on('click', function () {
-                    var tr = $(this).closest('tr');
-                    console.log(tr);
-                    tr.remove();
-                });
-            });            
+        
 
         }
 
