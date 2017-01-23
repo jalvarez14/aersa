e2638ed028918fec11f11bdd2058c2d539a59e99
Jorge <jalvarez@cicese.edu.mx>
2017-01-23 13:10:37 -0600

    aersa

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -1229,6 +1229,4 @@ return array(
     'zsdtBaseTask' => $vendorDir . '/phing/phing/classes/phing/tasks/ext/zendserverdeploymenttool/zsdtBaseTask.php',
     'zsdtPackTask' => $vendorDir . '/phing/phing/classes/phing/tasks/ext/zendserverdeploymenttool/zsdtPackTask.php',
     'zsdtValidateTask' => $vendorDir . '/phing/phing/classes/phing/tasks/ext/zendserverdeploymenttool/zsdtValidateTask.php',
-    'PHPReport' => $vendorDir .'/jasper/phpreport/PHPReport.php',
-    
 );
