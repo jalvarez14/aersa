c4f7931eb1a20bad2102a3bcf64a1f78854f5527
Jorge <jalvarez@cicese.edu.mx>
2017-01-23 10:38:03 -0600

    aersa

--- module/AERSA/src/Application/Reportes/Controller/ReportesController.php
+++ module/AERSA/src/Application/Reportes/Controller/ReportesController.php
@@ -430,6 +430,7 @@ class ReportesController extends AbstractActionController {
                 'templateDir' => $templateDir
             );
             $R = new \PHPReport($config);
+            var_dump($R);exit();
             $R->load(array(
                 array(
                     'id' => 'compania',
